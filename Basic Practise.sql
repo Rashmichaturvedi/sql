487c163e02e5e722d7c05b8761f7d5e2b2a0901e
Rashmi chaturvedi <rc1981999@gmail.com>
2020-03-15 23:31:01 +0530

    simple mysql commands (Studying theory part  more for sql)
    simple cmnds

---
+++ Basic Practise.sql
@@ -0,0 +1,15 @@
+CREATE TABLE Rashmi (
+    StudentId INTEGER,
+    FName CHAR(10), 
+    Gender CHAR(1), 
+    DOJ DATE);
+desc Rashmi       /*Display  table*/
+DROP TABLE Rashmi
+CREATE TABLE Sume(    /*Creating second tabe*/
+    StudentId INTEGER    NOT NULL,
+    FName VARCHAR(10) NOT NULL,
+    LName CHAR(10),
+DOJ DATE ,
+ MarksScored DECIMAL(5,2));
+desc Sume
+
